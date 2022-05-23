b64efdd2e997bb7e176819b05831344578a51e37
Davide Cristini <davcri91@gmail.com>
2022-05-23 09:19:29 +0200

    Remove unused code

--- addons/layouts/layout.gd
+++ addons/layouts/layout.gd
@@ -11,14 +11,3 @@ func _enter_tree():
 func _exit_tree():
 	if OS.is_debug_build():
 		remove_autoload_singleton("Layout")
-
-
-func add_project_settings():
-	ProjectSettings.set("category/property_name", 0)
-	var property_info = {
-		"name": "category/property_name",
-		"type": TYPE_INT,
-		"hint": PROPERTY_HINT_ENUM,
-		"hint_string": "one,two,three"
-	}
-	ProjectSettings.add_property_info(property_info)
